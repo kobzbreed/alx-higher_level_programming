1683c60545580f314e49a62c6b673bc17d849455
kobzbreed <jitekobimowe10@gmail.com>
2023-03-14 14:47:04 -0700

    advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  process.exit(1);
+}
+
+const files = process.argv.slice(2, process.argv.length - 1);
+const outputFile = process.argv[process.argv.length - 1];
+let data = '';
+
+for (const file of files) {
+  data += fs.readFileSync(file).toString();
+}
+fs.writeFile(outputFile, data, () => {});
