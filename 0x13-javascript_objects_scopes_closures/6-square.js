c95746f24e932c5adf11cc2fcce0c6ea1124c3fc
kobzbreed <jitekobimowe10@gmail.com>
2023-03-14 14:40:56 -0700

    javascript objects-scopes-closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const BaseSqaure = require('./5-square');
+
+class Square extends BaseSqaure {
+  charPrint (c) {
+    if (!c) {
+      this.print();
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log('');
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+Javascript objects_scopes_closures.
