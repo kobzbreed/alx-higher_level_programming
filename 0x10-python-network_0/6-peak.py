845a2fb6c626f4baa39a6eba6edc8925c0f7d2cf
kobzbreed <jitekobimowe10@gmail.com>
2023-03-30 15:43:03 -0700

    find the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 6-peak module """
+
+
+def find_peak(list_of_integers):
+    """ find the peak in a list of unsorted integers """
+
+    size = len(list_of_integers)
+    mid = size // 2
+    mid_a = size
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_a = mid_a // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_a // 2 == 0:
+                mid_a = 2
+        elif (mid_a > 0 and 
+                list_of_integers[mid] < list_of_integers[mid - 1]):
+            if mid_a // 2 == 0:
+                mid_a == 2
+            mid = mid - mid_a // 2
+        else:
+            return list_of_integers[mid]
