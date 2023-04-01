a581386fbd7c0a35335a7f9033437e515f4a91dd
kobzbreed <jitekobimowe10@gmail.com>
2023-04-01 14:44:51 -0700

    update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     """ Fetches https://alx-intranet.hbtn.io/status """
-    
+
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
