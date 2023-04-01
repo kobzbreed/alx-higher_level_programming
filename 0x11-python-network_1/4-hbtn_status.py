a69d574bc183b1b2165b3bb80dc1c0aeed5428f4
kobzbreed <jitekobimowe10@gmail.com>
2023-04-01 14:42:54 -0700

    add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+4-hbtn_status module
+"""
+import requests
+
+
+if __name__ == "__main__":
+    """ Fetches https://alx-intranet.hbtn.io/status """
+    
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
